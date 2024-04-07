892b9dd59c2f6e085ee42575577eb97411cd2a99
Simerl <33984591+SimeonLukas@users.noreply.github.com>
2024-04-07 21:43:37 +0200

    update

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@ use std::path::Path;
 use imageproc::drawing::{draw_text_mut, Canvas};
 use imageproc::geometric_transformations::*;
 use ab_glyph::FontArc;
+use std::io::Read;
 
 
 
@@ -24,24 +25,40 @@ async fn index(
     zeit: Option<String>
 
 ) -> Option<NamedFile> {
-    let font = FontArc::try_from_slice(include_bytes!("../fonts/Roboto-Regular.ttf")).unwrap();
+
+    // load json from static template
+    let mut template = String::new();
+    let mut file = std::fs::File::open("static/template2.json").unwrap();
+    file.read_to_string(&mut template).unwrap();
+    let template: serde_json::Value = serde_json::from_str(&template).unwrap();
+    let bg = template["config"][0]["background"].as_str().unwrap();
+
+    let roboto = FontArc::try_from_slice(include_bytes!("../fonts/Roboto-Regular.ttf")).unwrap();
+    let icons = FontArc::try_from_slice(include_bytes!("../fonts/icomoon.ttf")).unwrap();
     let ort = ort.unwrap_or("St. Martin Garmisch-Partenkirchen".to_string());
-    let zeit = zeit.unwrap_or("19:00 - 21:00 Uhr".to_string());
-    let bg = bg.unwrap_or("white.png".to_string());
+    let zeit = zeit.unwrap_or("".to_string());
+    // let bg = bg.unwrap_or("white.png".to_string());
     let img = ImageReader::open("static/".to_owned() + &bg).unwrap().decode().unwrap();
     let bg = img.to_rgba8();
     let width = bg.width();
     let height = bg.height();
     let mut extraimage = RgbaImage::from_pixel(width/2, height/2, Rgba([255, 255, 255, 0]));
-    draw_text_mut(&mut extraimage, image::Rgba([0,0,0, 255]), 100, 100, 50.0, &font , "test");
+    draw_text_mut(&mut extraimage, image::Rgba([0,0,0, 255]), 100, 100, 50.0, &roboto , "test");
     extraimage = rotate_about_center(&extraimage, 2.0, Interpolation::Bicubic, Rgba([255, 255, 255, 0]));
     let mut image: RgbaImage = ImageBuffer::new(width, height);
     image.copy_from(&bg, 0, 0).unwrap();
-    draw_text_mut(&mut image, image::Rgba([255, 255, 255, 255]), 130, 1000, 50.0, &font , &ort);
-    draw_text_mut(&mut image, image::Rgba([255, 255, 255, 255]), 130, 1110, 50.0, &font , &zeit);
+    draw_text_mut(&mut image, image::Rgba([255, 255, 255, 255]), 130, 1000, 50.0, &roboto , &ort);
+    draw_text_mut(&mut image, image::Rgba([255, 255, 255, 255]), 130, 1110, 50.0, &icons , &zeit);
 
 
     overlay(&mut image, &extraimage, 100, 100);
+
+
+
+
+
+
+
 
     if let Some(title) = title {
         let path = Path::new(&title);
@@ -54,7 +71,10 @@ async fn index(
         return Some(NamedFile::open("output.png").await.unwrap());
     }
     
-}
+
+
+
+    }
 
 
 #[launch]
